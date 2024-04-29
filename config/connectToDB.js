55f48384916c05a01868257d6f3ba8d57ef8ec66
Hannah-Moon <59512737+Hannah-Moon@users.noreply.github.com>
2024-04-29 12:56:09 -0400

    fix syntext error on config file.

--- config/connectToDB.js
+++ config/connectToDB.js
@@ -1,10 +1,13 @@
+// Load DB_URL variable from .env
 require("dotenv").config()
 
+// Import mongoose framework
 const mongoose = require('mongoose')
 
+ // Connect to mongoose via connection string in env file
 const connectToDB = async() => {
     await mongoose.connect(process.env.DB_URL);
     console.log("Currently Connected to MongoDB Cluster.")
 }
 
-module.export = connectToDB
+module.exports = connectToDB
